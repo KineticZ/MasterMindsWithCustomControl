5878e1cbdb0f30ed0f97f5e203bf83916754731a
Zanlin Oo <zanlin87@gmail.com>
2020-09-21 10:35:39 -0700

    Add project files.

---
+++ MasterMinds/Controls/ColorSelector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MasterMinds.Controls
+{
+    public partial class ColorSelector : UserControl
+    {
+        public Color[] colors;
+        public ColorSelector()
+        {
+            InitializeComponent();
+            textBox1.Text = "Select Color";
+        }
+        private void ColorSelector_Load(object sender, EventArgs e)
+        {
+            colors = new Color[]
+            {
+                Color.Red,
+                Color.Blue,
+                Color.Green
+            };
+
+            comboBox1.Items.AddRange(colors.Cast<object>().ToArray());
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+            => panel1.BackColor = (Color)((sender as ComboBox).SelectedItem);
+       
+    }
+}
